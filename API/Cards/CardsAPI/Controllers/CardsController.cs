c3a29d739509a78057108a0544c61546e1c852da
0lovka <93045406+0lovka@users.noreply.github.com>
2023-01-12 15:23:55 +0100

    Add files via upload

---
+++ API/Cards/CardsAPI/Controllers/CardsController.cs
@@ -0,0 +1,93 @@
+﻿using CardsAPI.Data;
+using CardsAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CardsAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CardsController : Controller
+    {
+        private readonly CardsDbContext cardsDbContext;
+
+        public CardsController(CardsDbContext cardsDbContext)
+        {
+            this.cardsDbContext = cardsDbContext;
+        }
+
+        //Get All Cards
+        [HttpGet]
+        public async Task<IActionResult> GetAllCards()
+        {
+            var cards = await cardsDbContext.Cards.ToListAsync();
+            return Ok(cards);
+        }
+
+        //Get Single Card
+        [HttpGet]
+        [Route("{id:guid}")]
+        [ActionName("GetSingleCard")]
+        public async Task<IActionResult> GetSingleCard([FromRoute] Guid id)
+        {
+            var card = await cardsDbContext.Cards.FirstOrDefaultAsync(x => x.Id == id);
+            if (card != null)
+                return Ok(card);
+            else
+                return NotFound("Card not found!");
+        }
+        
+        //Post Card
+        [HttpPost]
+        public async Task<IActionResult> AddCard([FromBody] Card card)
+        {
+            card.Id = Guid.NewGuid();
+            await cardsDbContext.Cards.AddAsync(card);
+            await cardsDbContext.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetSingleCard), new { id = card.Id }, card);
+        }
+
+        //Update Card
+        [HttpPut]
+        [Route("{id:guid}")]
+        public async Task<IActionResult> UpdateCard([FromRoute] Guid id, [FromBody] Card card)
+        {
+            var existingCard = await cardsDbContext.Cards.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (existingCard != null)
+            {
+                existingCard.CardHolderName = card.CardHolderName;
+                existingCard.CardNumber = card.CardNumber;
+                existingCard.ExpiryMonth = card.ExpiryMonth;
+                existingCard.ExpiryYear = card.ExpiryYear;
+                existingCard.CVC = card.CVC;
+
+                await cardsDbContext.SaveChangesAsync();
+
+                return Ok(existingCard);
+            }
+            else
+                return NotFound("Card not found!");
+        }
+
+        //Delete Card
+        [HttpDelete]
+        [Route("{id:guid}")]
+        public async Task<IActionResult> DeleteCard([FromRoute] Guid id)
+        {
+            var existingCard = await cardsDbContext.Cards.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (existingCard != null)
+            {
+                cardsDbContext.Remove(existingCard);
+
+                await cardsDbContext.SaveChangesAsync();
+
+                return Ok(existingCard);
+            }
+            else
+                return NotFound("Card not found!");
+        }
+    }
+}
---
+++ API/Cards/CardsAPI/Data/CardsDbContext.cs
@@ -0,0 +1,17 @@
+﻿using CardsAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CardsAPI.Data
+{
+    public class CardsDbContext : DbContext
+    {
+        //Constructor
+        public CardsDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        //DbSet - Tablica Cards u SQLu 
+        public DbSet<Card> Cards { get; set; }
+
+    }
+}
